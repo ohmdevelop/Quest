3b6a2d7cd5aa6813e500f0312a6838c30e0f31f2
nutzatvchZ <60559369+nutzatvchZ@users.noreply.github.com>
2020-02-02 09:03:29 +0700

    Update Loader.php

--- Quest/src/Quest/Loader.php
+++ Quest/src/Quest/Loader.php
@@ -10,4 +10,13 @@ use Quest\FormAPI\Form\CustomForm;
 use Quest \FormAPI\Form\SimpleForm;
 use Quest\FormAPI\Form\ModalForm;
 
-class Loader extends PluginBase{
+class Loader extends PluginBase implements Listener{
+      public eco;
+      public function onEnable(){
+         $this->eco = $this->getServer()->getPluginManager()->getPlugin('EconomyAPI");
+         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+       }
+           public function onCommand(CommandSender $p, Command $cmd, array $args, label $string): bool{
+           
+          }
+}
